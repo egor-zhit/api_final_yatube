f30026f673479d19eeab46fa6bc2a741a3b1dcd6
Егор Житников <zhitnikoveg@yandex.ru>
2022-09-23 17:32:30 +0300

    10

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import filters, permissions, status, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from .permissions import IsOwnerOrReadOnly
+from .permission import IsOwnerOrReadOnly
 from .serializers import (
     CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer)
 
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -16,47 +16,53 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    group = models.ForeignKey(Group,
-                              on_delete=models.SET_NULL,
-                              related_name='posts',
-                              blank=True, null=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(Group,
+                              on_delete=models.SET_NULL,
+                              related_name='posts',
+                              blank=True,
+                              null=True,)
 
     def __str__(self):
         return self.text
 
 
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
+
+    def __str__(self):
+        return '"{}" to post "{}" by author "{}"'.format(self.text,
+                                                         self.post,
+                                                         self.author)
+
+
 class Follow(models.Model):
     user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='follower',
-        verbose_name='Подписчик',
+        User, on_delete=models.CASCADE, null=True, blank=True,
+        related_name='follower'
     )
     following = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following',
-        verbose_name='Подписан',
+        User, on_delete=models.CASCADE, null=True, blank=True,
+        related_name='following'
     )
 
     class Meta:
-        constraints = (
-            models.UniqueConstraint(
-                fields=('user', 'following'),
-                name='unique_follow'
-            ),
-        )
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'following'],
+                                    name='unique_user_subscribers')
+        ]
 
-
-class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+    def __str__(self):
+        return '{} follows {}'.format(self.user, self.following)
