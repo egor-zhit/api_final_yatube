5c0556ae1f00fde801b119cb748bc84c5c2fd54c
Егор Житников <zhitnikoveg@yandex.ru>
2022-09-23 18:44:50 +0300

    q

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
@@ -14,6 +13,6 @@ router.register('groups', GroupViewSet)
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/auth/', include('djoser.urls')),
-    path('v1/token/', include('djoser.urls.jwt')),
-    path('v1/token/refresh/', include('djoser.urls.jwt'))
+    path('v1/jwt/create/', include('djoser.urls.jwt')),
+    path('v1/jwt/refresh/', include('djoser.urls.jwt'))
 ]
