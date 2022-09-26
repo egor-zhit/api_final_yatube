bc7197ccf43732b8db7dd5beb90f01ae6fae6ee1
Егор Житников <zhitnikoveg@yandex.ru>
2022-09-26 20:30:10 +0300

    12

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,20 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
-router.register('posts', PostViewSet, basename='posts')
-router.register('follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
-                basename='comments')
+router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet, basename='followers')
+router.register(r'^posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/', include('djoser.urls')),
-    path('v1/jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/jwt/refresh/', TokenRefreshView.as_view, name='token_refresh')
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -16,9 +16,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'django_filters',
     'djoser',
+    
     'api',
     'posts',
 ]
